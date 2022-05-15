831790fb24696be3051de8ff5c162282902dfa2f
Alex <79322837+Topal-Alex@users.noreply.github.com>
2022-05-15 13:44:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+
+
+namespace ConsoleApplication1
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveParentheses("a (bc d)e"));
+
+        }
+        public static string RemoveParentheses(string s)
+        {
+            
+            int first_p = 0;
+            int last_p = 0;
+            for(int i = 0;i < s.Length;i++)
+            {
+                if(s[i] =='(')
+                {
+                    first_p = i;
+                    break;
+                }
+            }
+            for(int j = s.Length-1;j>0;j--)
+            {
+                if(s[j] == ')')
+                {
+                    last_p = j;
+                    break;
+                }
+            }
+            int dlina_vireza = last_p - first_p + 1;
+            string rezult = s.Remove(first_p, dlina_vireza);
+            return rezult;
+        }
+            
+        }
+        
+
+    }
